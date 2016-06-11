d1b20d3bf673e483b81024ce5771236deda8f5eb
felix <felixmdev@gmail.com>
2016-06-11 15:00:31 -0700

    fonts in public folder

--- client/main.js
+++ client/main.js
@@ -2,24 +2,28 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-Template.admin.onCreated(function adminOnCreate(){
+Template.adminPanel.rendered(function (){
+Blaze._allowJavascriptUrls()
+  var uiHelperSlick = function(){
+       // Get each slider element (with .js-slider class)
+       jQuery('.js-slider').each(function(){
+           var $slider = jQuery(this);
 
-});
+           // Get each slider's init data
+           var $sliderArrows       = $slider.data('slider-arrows') ? $slider.data('slider-arrows') : false;
+           var $sliderDots         = $slider.data('slider-dots') ? $slider.data('slider-dots') : false;
+           var $sliderNum          = $slider.data('slider-num') ? $slider.data('slider-num') : 1;
+           var $sliderAuto         = $slider.data('slider-autoplay') ? $slider.data('slider-autoplay') : false;
+           var $sliderAutoSpeed    = $slider.data('slider-autoplay-speed') ? $slider.data('slider-autoplay-speed') : 3000;
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+           // Init slick slider
+           $slider.slick({
+               arrows: $sliderArrows,
+               dots: $sliderDots,
+               slidesToShow: $sliderNum,
+               autoplay: $sliderAuto,
+               autoplaySpeed: $sliderAutoSpeed
+           });
+       });
+   };
 });
